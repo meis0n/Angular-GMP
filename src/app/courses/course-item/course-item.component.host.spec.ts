b76aa9cbcc517ece0096cf86becd985857ebeba8
Sergei Moiseev <sergei_moiseev@epam.com>
2019-11-05 02:34:26 +0400

    task3: added several simple tests

---
+++ src/app/courses/course-item/course-item.component.host.spec.ts
@@ -0,0 +1,90 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseItemComponent } from './course-item.component';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { Course } from '../course';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `
+    <app-course-item
+      [data]="course"
+      (changed)="onChange($event)"
+      (deleted)="onDelete($event)"
+    ></app-course-item>
+  `
+})
+class TestHostCourseItemComponent {
+  public course: Course = {
+    creationDate: '12.10.10',
+    description: 'Lorem ipsum',
+    duration: '1h 12min',
+    id: '1',
+    title: 'Test course',
+  };
+
+  public onChange(changedCourse: Course) {
+    console.log(changedCourse);
+  }
+
+  public onDelete(deletedCourseId: string) {
+    console.log(deletedCourseId);
+  }
+}
+
+describe('TestHostCourseItemComponent', () => {
+  let component: TestHostCourseItemComponent;
+  let fixture: ComponentFixture<TestHostCourseItemComponent>;
+  let onDeleteSpy: jasmine.Spy;
+  let onChangeSpy: jasmine.Spy;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CourseItemComponent, TestHostCourseItemComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestHostCourseItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    onDeleteSpy = spyOn(component, 'onDelete').and.callThrough();
+    onChangeSpy = spyOn(component, 'onChange').and.callThrough();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  it('should call parent`s handler on "Edit" button ', () => {
+    const editButton = fixture.debugElement.query(By.css('[data-test-id="EditCourse"'));
+    editButton.triggerEventHandler('click', null);
+
+    expect(onChangeSpy).toHaveBeenCalled();
+    expect(onChangeSpy).toHaveBeenCalledWith(component.course);
+  });
+
+  it('should call parent`s handler on "Delete" button ', () => {
+    const deleteButton = fixture.debugElement.query(By.css('[data-test-id="DeleteCourse"'));
+    deleteButton.triggerEventHandler('click', null);
+
+    expect(onDeleteSpy).toHaveBeenCalled();
+    expect(onDeleteSpy).toHaveBeenCalledWith(component.course.id);
+  });
+
+  it('should renders passed title', () => {
+    const titleContainer = fixture.debugElement.query(By.css('[data-test-id="CourseTitle"')).nativeElement;
+
+    expect(titleContainer.textContent.trim()).toEqual(component.course.title);
+  });
+
+  it('should renders passed description', () => {
+    const descriptionContainer = fixture.debugElement.query(By.css('[data-test-id="CourseDescription"')).nativeElement;
+
+    expect(descriptionContainer.textContent.trim()).toEqual(component.course.description);
+  });
+});
--- src/app/courses/course-item/course-item.component.spec.ts
+++ src/app/courses/course-item/course-item.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseItemComponent } from './course-item.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CourseItemComponent', () => {
   let component: CourseItemComponent;
   let fixture: ComponentFixture<CourseItemComponent>;
+  let onDeleteSpy: jasmine.Spy;
+  let onEditSpy: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseItemComponent ]
+      declarations: [ CourseItemComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -17,9 +22,26 @@ describe('CourseItemComponent', () => {
     fixture = TestBed.createComponent(CourseItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    onDeleteSpy = spyOn(component, 'onDelete').and.callThrough();
+    onEditSpy = spyOn(component, 'onEdit').and.callThrough();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call handler on "Edit" button ', () => {
+    const editButton = fixture.debugElement.query(By.css('[data-test-id="EditCourse"'));
+    editButton.triggerEventHandler('click', null);
+
+    expect(onEditSpy).toHaveBeenCalled();
+  });
+
+  it('should call handler on "Delete" button ', () => {
+    const deleteButton = fixture.debugElement.query(By.css('[data-test-id="DeleteCourse"'));
+    deleteButton.triggerEventHandler('click', null);
+
+    expect(onDeleteSpy).toHaveBeenCalled();
   });
 });
